c4eff79ddaccfd0570b695acc924ab35c414d707
Mayor-Isaac <ogunyilekaferanmi@gmail.com>
2024-12-11 01:09:56 +0100

    Pages and Routing Implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Product from "./pages/Product";
+import Product from "./pages/Product.jsx";
+import Homepage from "./pages/Homepage.jsx";
+import Pricing from "./pages/Pricing.jsx";
+import PageNotFound from "./pages/PageNotFound.jsx";
+import AppLayout from "./pages/AppLayout.jsx";
+import Login from "./pages/Login.jsx";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="" element={<Homepage />} />
         <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
